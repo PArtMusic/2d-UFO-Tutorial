6511a91be8ed955c1f96c89fc96e9c44a3a09111
PArtMusic <PArtMusic91@gmail.com>
2017-05-23 22:16:59 -0400

    Completed videos 1-3

---
+++ 2D UFO Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector2 movement = new Vector2();
+    private Rigidbody2D rb2d;
+    public float speedMultiplier;
+
+
+    public void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        movement.x = moveHorizontal;
+        movement.y = moveVertical;
+        rb2d.AddForce(movement * speedMultiplier);
+    }
+
+}
