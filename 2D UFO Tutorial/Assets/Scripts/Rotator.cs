8669e528e9389105b82423301769ab61cfc6915e
PArtMusic <PArtMusic91@gmail.com>
2017-05-24 21:14:18 -0400

    Completed the next few videos
    Added collision with walls and collectables

---
+++ 2D UFO Tutorial/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    public Vector3 v3 = new Vector3(0,0,45);
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(v3 * Time.deltaTime);
+	}
+}
